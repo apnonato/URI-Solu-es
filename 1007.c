b844c44b8ac551544edf812e8463bd68656d4ea9
apnonato <83020348+apnonato@users.noreply.github.com>
2021-06-13 21:25:19 -0300

    Create 1007.c

---
+++ 1007.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  int A ; 
+  scanf("%d",&A);
+  int B;
+  scanf("%d",&B);
+  int C;
+  scanf("%d",&C);
+  int D;
+  scanf("%d",&D);
+  int diferenca = (A * B - C * D) ; 
+  printf("DIFERENCA = %d\n",diferenca); 
+  return 0;
+}
